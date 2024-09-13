e77a39d5a9391462aca1d4de6702660292d9a4f4
Leon-Leyang <leyang_hu@brown.edu>
2024-09-13 16:35:51 -0400

    Optimize the script

--- evaluation/prompt_optimization.py
+++ evaluation/prompt_optimization.py
@@ -2,32 +2,33 @@ import argparse
 import concurrent
 from dotenv import load_dotenv
 load_dotenv(override=True)
-
 from tqdm import tqdm
 import textgrad as tg
 from textgrad.tasks import load_task
-
 import numpy as np
 import random
+
 
 def set_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
 
+
 def config():
     parser = argparse.ArgumentParser(description="Optimize a prompt for a task.")
     parser.add_argument("--task", type=str, default="BBH_object_counting", help="The task to evaluate the model on.")
-    parser.add_argument("--evaluation_engine", type=str, default="gpt-4o", help="The API to use for evaluation.")
-    parser.add_argument("--test_engine", type=str, default="gpt-3.5-turbo-0125", help="The API to use for evaluation.")
+    parser.add_argument("--engine", type=str, default="llama-3", help="The API to use.")
+    parser.add_argument("--optimizer_version", type=str, default="v1", help="The optimizer to use.")
     parser.add_argument("--batch_size", type=int, default=3, help="The batch size to use for training.")
-    parser.add_argument("--max_epochs", type=int, default=3, help="The maximum number of epochs to train for.")
+    parser.add_argument("--max_epochs", type=int, default=1, help="The maximum number of epochs to train for.")
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--run_validation", action="store_true", help="Whether to run validation or not.")
-    parser.add_argument("--do_not_run_larger_model", action="store_true", help="Whether to run the larger model or not.")
-    parser.add_argument("--num_threads", type=int, default=32, help="The number of threads to use for evaluation.")
+    parser.add_argument("--num_threads", type=int, default=3, help="The number of threads to use for evaluation.")
     return parser.parse_args()
 
+
 args = config()
+
 
 def eval_sample(item, eval_fn, model):
     x, y = item
@@ -62,15 +63,23 @@ def eval_dataset(test_set, eval_fn, model, max_samples: int=None):
             tqdm_loader.set_description(f"Accuracy: {np.mean(accuracy_list)}")
     return accuracy_list 
 
+
 def run_validation_revert(system_prompt: tg.Variable, results, model, eval_fn, val_set):
-    val_performance = np.mean(eval_dataset(val_set, eval_fn, model))
+    if system_prompt.get_value() not in val_acc_cache:
+        print("Running the current prompt on the validation set...")
+        val_acc_list = eval_dataset(val_set, eval_fn, model)
+        val_acc_cache[system_prompt.get_value()] = val_acc_list
+        val_performance = np.mean(val_acc_list)
+    else:
+        print("Using cached value for the current prompt on the validation set...")
+        val_performance = np.mean(val_acc_cache[system_prompt.get_value()])
     previous_performance = np.mean(results["validation_acc"][-1])
-    print("val_performance: ", val_performance)
-    print("previous_performance: ", previous_performance)
+    print("Val acc: ", val_performance)
+    print("Previous acc: ", previous_performance)
     previous_prompt = results["prompt"][-1]
     
     if val_performance < previous_performance:
-        print(f"rejected prompt: {system_prompt.value}")
+        print(f"Rejected prompt: {system_prompt.value}")
         system_prompt.set_value(previous_prompt)
         val_performance = previous_performance
 
@@ -78,69 +87,97 @@ def run_validation_revert(system_prompt: tg.Variable, results, model, eval_fn, v
 
 
 set_seed(args.seed)
-llm_api_eval = tg.get_engine(engine_name=args.evaluation_engine)
-llm_api_test = tg.get_engine(engine_name=args.test_engine)
-tg.set_backward_engine(llm_api_eval, override=True)
+if 'llama' in args.engine:
+    llm_api = tg.get_engine(engine_name=args.engine, batch_size=args.num_threads)
+else:
+    llm_api = tg.get_engine(engine_name=args.engine)
+tg.set_backward_engine(llm_api, override=True)
 
 # Load the data and the evaluation function
-train_set, val_set, test_set, eval_fn = load_task(args.task, evaluation_api=llm_api_eval)
+train_set, val_set, test_set, eval_fn = load_task(args.task, evaluation_api=llm_api)
 print("Train/Val/Test Set Lengths: ", len(train_set), len(val_set), len(test_set))
 STARTING_SYSTEM_PROMPT = train_set.get_task_description()
 
 train_loader = tg.tasks.DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
-print(STARTING_SYSTEM_PROMPT)
-
-
-# Testing the 0-shot performance of the evaluation engine
-system_prompt = tg.Variable(STARTING_SYSTEM_PROMPT, 
-                            requires_grad=True, 
-                            role_description="system prompt to the language model")
-model_evaluation = tg.BlackboxLLM(llm_api_eval, system_prompt)
-
-if not args.do_not_run_larger_model:
-    reference = np.mean(eval_dataset(test_set, eval_fn, model_evaluation))
-
+print(f'Starting system prompt: {STARTING_SYSTEM_PROMPT}')
 
 system_prompt = tg.Variable(STARTING_SYSTEM_PROMPT, 
                             requires_grad=True,
                             role_description="structured system prompt to a somewhat capable language model that specifies the behavior and strategies for the QA task")
-model = tg.BlackboxLLM(llm_api_test, system_prompt)
+model = tg.BlackboxLLM(llm_api, system_prompt)
 
-optimizer = tg.TextualGradientDescent(engine=llm_api_eval, parameters=[system_prompt])
+if args.optimizer_version == "v1":
+    optimizer = tg.TextualGradientDescent(engine=llm_api, parameters=[system_prompt])
+elif args.optimizer_version == "v2":
+    optimizer = tg.TextualGradientDescentV2(engine=llm_api, parameters=[system_prompt])
+else:
+    raise ValueError(f"Invalid optimizer version: {args.optimizer_version}")
 
 results = {"test_acc": [], "prompt": [], "validation_acc": []}
+print("Evaluating the initial prompt on the test set...")
 results["test_acc"].append(eval_dataset(test_set, eval_fn, model))
+print("Evaluating the initial prompt on the validation set...")
 results["validation_acc"].append(eval_dataset(val_set, eval_fn, model))
 results["prompt"].append(system_prompt.get_value())
 
+# Cache the val/test accuracy for each system prompt
+val_acc_cache = {system_prompt.get_value(): results["validation_acc"][-1]}
+test_acc_cache = {system_prompt.get_value(): results["test_acc"][-1]}
 
 for epoch in range(args.max_epochs):
     for steps, (batch_x, batch_y) in enumerate((pbar := tqdm(train_loader, position=0))):
         pbar.set_description(f"Training step {steps}. Epoch {epoch}")
         optimizer.zero_grad()
         losses = []
-        for (x, y) in zip(batch_x, batch_y):
-            x = tg.Variable(x, requires_grad=False, role_description="query to the language model")
-            y = tg.Variable(y, requires_grad=False, role_description="correct answer for the query")
-            response = model(x)
-            try:
-                eval_output_variable = eval_fn(inputs=dict(prediction=response, ground_truth_answer=y))
-            except:
-                eval_output_variable = eval_fn([x, y, response])
-            losses.append(eval_output_variable)
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=args.num_threads) as executor:
+            futures = []
+            for (x, y) in zip(batch_x, batch_y):
+                future = executor.submit(get_eval_ouput, x, y, model, eval_fn)
+                futures.append(future)
+                if len(futures) >= max_samples:
+                    break
+            tqdm_loader = tqdm(concurrent.futures.as_completed(futures), total=len(futures), position=0)
+            for future in tqdm_loader:
+                eval_output_variable = future.result()
+                losses.append(eval_output_variable)
+
         total_loss = tg.sum(losses)
         total_loss.backward()
         optimizer.step()
         if args.run_validation:
             run_validation_revert(system_prompt, results, model, eval_fn, val_set)
-        print("sys prompt: ", system_prompt)
-        test_acc = eval_dataset(test_set, eval_fn, model)
+        print("Current sys prompt: ", system_prompt)
+        if system_prompt.get_value() not in test_acc_cache:
+            print("Evaluating the current prompt on the test set...")
+            test_acc = eval_dataset(test_set, eval_fn, model)
+            test_acc_cache[system_prompt.get_value()] = test_acc
+        else:
+            print("Using cached value for the current prompt on the test set...")
+            test_acc = test_acc_cache[system_prompt.get_value()]
         results["test_acc"].append(test_acc)
         results["prompt"].append(system_prompt.get_value())
-        if steps == 3:
+        if steps == 12:
             break
+
+
+def get_eval_ouput(x, y, model, eval_fn):
+    """
+    Helper function to be used in the ThreadPoolExecutor to evaluate the model on a batch of data.
+    """
+    x = tg.Variable(x, requires_grad=False, role_description="query to the language model")
+    y = tg.Variable(y, requires_grad=False, role_description="correct answer for the query")
+    response = model(x)
+    try:
+        eval_output_variable = eval_fn(inputs=dict(prediction=response, ground_truth_answer=y))
+        return int(eval_output_variable.value)
+    except:
+        eval_output_variable = eval_fn([x, y, response])
+        eval_output_parsed = eval_fn.parse_output(eval_output_variable)
+        return int(eval_output_parsed)
+
 
 # Also dump the final results
 import json
-with open(f"./figures/results_{args.task}_{args.test_engine}.json", "w") as f:
+with open(f"./figures/results_{args.task}_{args.engine}_{args.optimizer_version}.json", "w") as f:
     json.dump(results, f)
