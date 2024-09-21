dd4984ac558f70f223fdb82d1708bf3ff711472a
Leon-Leyang <leyang_hu@brown.edu>
2024-09-21 18:07:38 -0400

    Update num_threads

--- evaluation/scripts/prompt_gsm8k.sh
+++ evaluation/scripts/prompt_gsm8k.sh
@@ -5,11 +5,11 @@ python -u ./evaluation/prompt_optimization.py \
 --engine llama-3_1 \
 --optimizer_version v1 \
 --run_validation \
---num_threads 30 \
+--num_threads 10 \
 
 python -u ./evaluation/prompt_optimization.py \
 --task GSM8K_DSPy \
 --engine llama-3_1 \
 --optimizer_version v2 \
 --run_validation \
---num_threads 30 \
+--num_threads 5 \
