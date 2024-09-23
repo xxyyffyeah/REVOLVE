5c95ebc4df66c7948dc84bc5b4111542b39212b5
Leon-Leyang <leyang_hu@brown.edu>
2024-09-22 23:04:57 -0400

    Update script

--- evaluation/scripts/prompt_gsm8k.sh
+++ evaluation/scripts/prompt_gsm8k.sh
@@ -2,14 +2,17 @@ export PYTHONPATH=$(pwd)
 
 python -u ./evaluation/prompt_optimization.py \
 --task GSM8K_DSPy \
---engine llama-3_1 \
+--engine meta-llama/Meta-Llama-3.1-70B-Instruct \
 --optimizer_version v1 \
 --run_validation \
 --num_threads 10 \
 
 python -u ./evaluation/prompt_optimization.py \
 --task GSM8K_DSPy \
---engine llama-3_1 \
+--engine meta-llama/Meta-Llama-3.1-70B-Instruct \
 --optimizer_version v2 \
 --run_validation \
---num_threads 5 \
+--num_threads 10 \
+
+python -u ./evaluation/gsm8k_dspy.py \
+--model meta-llama/Meta-Llama-3.1-70B-Instruct
