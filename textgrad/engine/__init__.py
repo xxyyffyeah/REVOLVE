78e76267f567899e9a47b6ed342fb1faf69d3a72
Peiyan Zhang <peiyance@gmail.com>
2024-09-23 20:45:45 +0800

    Update __init__.py

--- textgrad/engine/__init__.py
+++ textgrad/engine/__init__.py
@@ -50,7 +50,7 @@ def get_engine(engine_name: str, **kwargs) -> EngineLM:
         return ChatGemini(model_string=engine_name, **kwargs)
     elif "llama-3" in engine_name:
         from .llama3_1 import ChatLlama3_1
-        return ChatLlama3_1(model_string="meta-llama/Meta-Llama-3.1-8B-Instruct", **kwargs)
+        return ChatLlama3_1(model_string=engine_name, **kwargs)
     elif "together" in engine_name:
         from .together import ChatTogether
         engine_name = engine_name.replace("together-", "")
