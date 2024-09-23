12d31db2dff715eed843541e7dd20ca31cbae372
Allen Bartholomew <1186546495@qq.com>
2024-09-22 21:49:30 -0500

    fixed the code optimization issue2

---
+++ solution_optimization_result/evaluation/scripts/prompt_gsm8k.sh
@@ -0,0 +1,15 @@
+export PYTHONPATH=$(pwd)
+
+python -u ./evaluation/prompt_optimization.py \
+--task GSM8K_DSPy \
+--engine llama-3_1 \
+--optimizer_version v1 \
+--run_validation \
+--num_threads 30 \
+
+python -u ./evaluation/prompt_optimization.py \
+--task GSM8K_DSPy \
+--engine llama-3_1 \
+--optimizer_version v2 \
+--run_validation \
+--num_threads 30 \
